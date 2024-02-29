58a401a2f174dc982a4d6ecd7ea49c98eccc79be
Spyros Triantafyllis <striantafyllis@gmail.com>
2024-02-29 15:23:36 -0800

    added MUS 291

--- sync_music_to_aws.sh
+++ sync_music_to_aws.sh
@@ -3,6 +3,6 @@
 for dir in 'personal' 'recordings' 'MUS 290' 'MUS 291' 'PioneerDJ' 'Old Music'
 do
   echo "Syncing $dir"
-  echo aws s3 sync --delete ~/Music/"$dir" s3://spyros-personal2/Music/"$dir"
-  time aws s3 sync --delete ~/Music/"$dir" s3://spyros-personal2/Music/"$dir"
+  echo aws s3 sync --delete --exclude '.DS_Store' ~/Music/"$dir" s3://spyros-personal2/Music/"$dir"
+  time aws s3 sync --delete --exclude '.DS_Store' ~/Music/"$dir" s3://spyros-personal2/Music/"$dir"
 done

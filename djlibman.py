ae585d8b1aaf31b772ea89bd18d7222ce17fd875
Spyros Triantafyllis <striantafyllis@gmail.com>
2024-06-21 20:41:36 -0700

    This can now read google sheets and run an interactive shell.

--- djlibman.py
+++ djlibman.py
@@ -5,10 +5,24 @@ import argparse
 import configparser
 import os
 import os.path
+import readline
 import code
+import time
 
 import google_interface
 import spotify_interface
+
+import pandas as pd
+
+def test_func():
+    print('Test')
+    return 'Test'
+
+should_quit = False
+
+def quit():
+    global should_quit
+    should_quit = True
 
 class Context:
     def __init__(self):
@@ -18,11 +32,11 @@ class Context:
         self.docs = {}
         return
 
-    def add_google_sheet(self, names, id, page, has_header=False):
+    def add_google_sheet(self, names, id, page, has_header=False, column_types = {}):
         if self.google is None:
             raise Exception("Cannot add Google sheet '%s'; no Google connection specified" % names[0])
 
-        doc = google_interface.GoogleSheet(self.google, id, page, has_header)
+        doc = google_interface.GoogleSheet(self.google, id, page, has_header, column_types)
 
         for name in names:
             if name in self.docs:
@@ -30,6 +44,83 @@ class Context:
             self.docs[name] = doc
 
         return
+
+def get_column_types(config):
+    column_types = {}
+
+    for key in config:
+        if key.startswith('type.'):
+            column_name = key[5:]
+
+            type_name = config[key]
+
+            if type_name.upper() in ['INT', 'INTEGER']:
+                type = int
+            elif type_name.upper() in ['FLOAT', 'DOUBLE']:
+                type = float
+            elif type_name.upper() in ['BOOL', 'BOOLEAN']:
+                type = bool
+            elif type_name.upper() == 'LIST':
+                type = list
+            elif type_name.upper() in ['DATE', 'TIMESTAMP']:
+                type = pd.Timestamp
+            else:
+                raise Exception("Unrecognized type name '%s'" % type_name)
+
+            if column_name in column_types:
+                raise Exception("Duplicate column type: '%s'" % column_name)
+
+            column_types[column_name] = type
+
+    return column_types
+
+
+def python_shell(shell_locals):
+    global should_quit
+
+    ic = code.InteractiveConsole(shell_locals)
+
+    # This code is taken from code.InteractiveConsole.interact().
+    # It is modified to have better behavior around errors
+
+    ic.write('Welcome to djlibman! Type quit() to exit\n')
+    cprt = 'Type "help", "copyright", "credits" or "license" for more information.'
+    ic.write("Python %s on %s\n%s\n(%s)\n" %
+               (sys.version, sys.platform, cprt,
+                'InteractiveConsole'))
+
+    ps1 = ">>> "
+    ps2 = "... "
+
+    more = False
+    while not should_quit:
+        try:
+            if more:
+                prompt = ps2
+            else:
+                prompt = ps1
+            try:
+                # all of this complicated mess is required for the prompt to appear after
+                # the syntax error (if there is one). This may be a side effect of the IDE...
+                sys.stderr.flush()
+                time.sleep(0.1)
+                sys.stdout.write(prompt)
+                sys.stdout.flush()
+                line = ic.raw_input('')
+            except EOFError:
+                ic.write("\n")
+                break
+            else:
+                more = ic.push(line)
+        except KeyboardInterrupt:
+            ic.write("\nKeyboardInterrupt\n")
+            ic.resetbuffer()
+            more = False
+
+    ic.write('Exiting djlibman shell...\n')
+    return
+
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -78,22 +169,36 @@ def main():
                 id = section['id']
                 page = section['page']
                 has_header = section.getboolean('header')
+                column_types = get_column_types(section)
 
-                ctx.add_google_sheet(names, id, page, has_header)
+                ctx.add_google_sheet(names, id, page, has_header, column_types)
             else:
                 raise Exception("Unsupported doc type: '%s'" % type)
 
         else:
             raise Exception("Unrecognized section: '%s'" % section_name)
 
-    code.interact(
-        banner='Welcome to djlibman!',
-        local=locals()
-    )
+    # build the local vars of the console
+
+    # first add all the docs; in practice, only docs whose names are valid identifiers
+    # will be accessible - the rest will be accessible through ctx itself
+
+    shell_locals = dict(ctx.docs)
+
+    # add ctx itself, plus google, rekordbox and spotify; note that any docs with
+    # conflicting names will be overwritten
+    shell_locals['ctx'] = ctx
+    shell_locals['google'] = ctx.google
+    # TODO add spotify, rekordbox etc.
+
+    # finally, add all the globals; this gives us all the functions etc.
+    # notice again that any conflicting document names will be overwritten
+    shell_locals.update(globals())
+
+    # drop into a shell
+    python_shell(shell_locals)
 
     return 0
 
 if __name__ == '__main__':
     sys.exit(main())
-
-
--- google_interface.py
+++ google_interface.py
@@ -2,6 +2,8 @@ import os.path
 import re
 import sys
 
+import pandas as pd
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -69,11 +71,12 @@ class GoogleInterface:
         return self._connection
 
 class GoogleSheet:
-    def __init__(self, interface, id, page, has_header=False):
+    def __init__(self, interface, id, page, has_header=False, column_types = {}):
         self._interface = interface
         self._id = id
         self._page = page
         self._has_header = has_header
+        self._column_types = column_types
         return
 
     def read(self):
@@ -83,6 +86,64 @@ class GoogleSheet:
 
         values = result.get('values', [])
 
+        num_rows = len(values)
+        num_columns = max([len(row) for row in values])
+
+        if self._has_header:
+            if num_rows < 1:
+                raise Exception("Google sheet '%s' page '%s': header expected but no rows" %
+                                (self._id, self._page))
+            column_names = values[0]
+
+            if len(column_names) != num_columns:
+                raise Exception("Google sheet '%s' page '%s': %d columns in header but %d columns in values" %
+                                (self._id, self._page, len(column_names), num_columns))
+
+
+            values = values[1:]
+            num_rows -= 1
+        else:
+            column_names = [_col_num_to_alpha(col_num) for col_num in range(num_columns)]
+
+        # a bit of a mess here because config column names come out in lowercase
+        column_names_lower = [column_name.lower() for column_name in column_names]
+        for column_name in self._column_types:
+            if column_name not in column_names_lower:
+                raise Exception("Google sheet '%s' page '%s': column '%s' has a declared type but is not present in header" %
+                                (self._id, self._page, column_name))
+
+        columns = {}
+
+        for col_num in range(num_columns):
+            column_name = column_names[col_num]
+            column_type = self._column_types.get(column_name.lower())
+            column_values = [None] * num_rows
+
+            for row_num in range(num_rows):
+                row = values[row_num]
+                if col_num >= len(row):
+                    continue
+
+                value = row[col_num]
+
+                if column_type is not None:
+                    if column_type == list:
+                        value = [s.strip() for s in value.split(',')]
+                    else:
+                        try:
+                            value = column_type(value)
+                        except ValueError:
+                            raise Exception(
+                                "Google sheet '%s' page '%s' column %s row %d: value '%s'; expected %s" %
+                                (self._id, self._page, column_name, row_num, value, column_type))
+
+                column_values[row_num] = value
+
+            columns[column_name] = pd.Series(column_values)
+
+        df = pd.DataFrame(data=columns)
+
+        return df
 
 
     def write(self, df):

3b41d832625aaa7cc15402af7ba1abfce44ac8f7
Spyros Triantafyllis <striantafyllis@gmail.com>
2024-10-19 12:48:47 -0700

    Some reorg of playlist code to make it possible to create playlists in new ways

--- playlist_scripts.py
+++ playlist_scripts.py
@@ -19,7 +19,7 @@ def has_value(list_field, *values):
     return False
 
 
-_playlists = {
+_playlist_conditions = {
     'ALL 120':
         lambda track: track.BPM >= 113,
 
@@ -57,118 +57,189 @@ _playlists = {
         lambda track: has_value(track.Playlists, 'feels') and is_class(track, 'A', 'B')
 }
 
-def build_playlist(name,
-                   condition=None,
-                   print_tracks=True,
-                   do_rekordbox=False,
-                   do_spotify=False,
-                   rekordbox_folder=['managed'],
-                   rekordbox_prefix='',
-                   rekordbox_overwrite=True,
-                   spotify_prefix='DJ ',
-                   spotify_overwrite=True):
-    global _playlists
+def create_playlist_from_condition(
+    name,
+    condition=None,
+    print_tracks=True,
+    do_rekordbox=False,
+    do_spotify=False,
+    rekordbox_folder=['managed'],
+    rekordbox_prefix='',
+    rekordbox_overwrite=True,
+    spotify_prefix='DJ ',
+    spotify_overwrite=True):
+
+    global _playlist_conditions
 
     if condition is None:
-        condition = _playlists[name]
+        condition = _playlist_conditions[name]
 
     djlib_tracks = docs['djlib'].read()
 
     tracks_filter = djlib_tracks.apply(condition, axis=1)
 
-    djlib_playlist = djlib_tracks.loc[tracks_filter]
+    track_ids_df = djlib_tracks.loc[tracks_filter]
 
-    print("Playlist '%s': %d tracks" % (name, len(djlib_playlist)))
+    create_playlist(track_ids_df,
+                    print_tracks,
+                    do_rekordbox,
+                    do_spotify,
+                    rekordbox_folder,
+                    rekordbox_prefix,
+                    rekordbox_overwrite,
+                    spotify_prefix,
+                    spotify_overwrite)
+
+    return
+
+
+def create_playlist(
+        name,
+        track_ids_df,
+        print_tracks=True,
+        do_rekordbox=False,
+        do_spotify=False,
+        rekordbox_folder=['managed'],
+        rekordbox_prefix='',
+        rekordbox_overwrite=True,
+        spotify_prefix='DJ ',
+        spotify_overwrite=True):
+
+    print("Playlist '%s': %d tracks" % (name, len(track_ids_df)))
     if print_tracks:
-        pretty_print_tracks(djlib_playlist, indent=' '*4, enum=True)
+        pretty_print_tracks(track_ids_df, indent=' ' * 4, enum=True)
 
     if do_rekordbox:
-        rekordbox_name = rekordbox_prefix + name
-        if rekordbox_folder is None:
-            rekordbox_full_name = [rekordbox_name]
-        elif isinstance(rekordbox_folder, str):
-            rekordbox_full_name = [rekordbox_folder, rekordbox_name]
-        elif isinstance(rekordbox_folder, list):
-            rekordbox_full_name = rekordbox_folder + [rekordbox_name]
-        else:
-            raise ValueError(rekordbox_folder)
-
-        print()
-        print('Creating Rekordbox playlist %s' % rekordbox_full_name)
-        rekordbox.create_playlist(rekordbox_full_name, djlib_playlist, overwrite=rekordbox_overwrite)
-        rekordbox.write()
-
-    if do_spotify:
-        rekordbox_to_spotify_mapping = docs['rekordbox_to_spotify'].read()
-
-        # remove empty mappings
-        rekordbox_to_spotify_mapping = rekordbox_to_spotify_mapping.loc[
-            ~pd.isna(rekordbox_to_spotify_mapping.spotify_id)
-        ]
-
-        djlib_playlist_with_spotify = djlib_playlist.merge(
-            rekordbox_to_spotify_mapping,
-            how='inner',
-            left_index=True,
-            right_index=True
+        create_rekordbox_playlist(
+            name,
+            track_ids_df,
+            print_tracks=False,
+            folder=rekordbox_folder,
+            prefix=rekordbox_prefix,
+            overwrite=rekordbox_overwrite
         )
 
-        if len(djlib_playlist_with_spotify) < len(djlib_playlist):
-            print('%d tracks are missing Spotify mappings; omitting.' % (
-                len(djlib_playlist) - len(djlib_playlist_with_spotify)
-            ))
-
-        spotify_playlist_name = spotify_prefix + name
-
-        spotify_playlists = spotify.get_playlists()
-
-        if spotify_playlist_name in spotify_playlists.index:
-            spotify_playlist_id = spotify_playlists.at[spotify_playlist_name, 'id']
-
-            if not spotify_overwrite:
-                raise Exception("Spotify playlist '%s' already exists" % spotify_playlist_name)
-
-            spotify_playlist_tracks = spotify.get_playlist_tracks(spotify_playlist_id)
-
-            spotify_ids_to_remove = spotify_playlist_tracks.index.difference(
-                djlib_playlist_with_spotify.spotify_id,
-                sort=False
-            )
-
-            spotify_ids_to_add = pd.Index(djlib_playlist_with_spotify.spotify_id).difference(
-                spotify_playlist_tracks.index,
-                sort=False
-            )
-
-            print("Spotify playlist '%s' exists with %d tracks; removing %d tracks, adding %d tracks" % (
-                spotify_playlist_name,
-                len(spotify_playlist_tracks),
-                len(spotify_ids_to_remove),
-                len(spotify_ids_to_add)
-            ))
-
-            if len(spotify_ids_to_remove) > 0:
-                spotify.remove_tracks_from_playlist(spotify_playlist_id, spotify_ids_to_remove)
-            if len(spotify_ids_to_add) > 0:
-                spotify.add_tracks_to_playlist(spotify_playlist_id, spotify_ids_to_add)
-
-        else:
-            print("Creating Spotify playlist '%s' with %d tracks" % (
-                spotify_playlist_name,
-                len(djlib_playlist_with_spotify)
-            ))
-
-            spotify.create_playlist(spotify_playlist_name)
-            spotify.add_tracks_to_playlist(spotify_playlist_name, djlib_playlist_with_spotify.spotify_id)
+    if do_spotify:
+        create_spotify_playlist(
+            name,
+            track_ids_df,
+            print_tracks=False,
+            spotify_prefix=spotify_prefix,
+            spotify_overwrite=spotify_overwrite
+        )
 
     print()
 
     return
 
 
+def create_rekordbox_playlist(
+    name,
+    track_ids_df,
+    print_tracks=True,
+    folder=['managed'],
+    prefix='',
+    overwrite=True):
+
+    rekordbox_name = prefix + name
+    if folder is None:
+        rekordbox_full_name = [rekordbox_name]
+    elif isinstance(folder, str):
+        rekordbox_full_name = [folder, rekordbox_name]
+    elif isinstance(folder, list):
+        rekordbox_full_name = folder + [rekordbox_name]
+    else:
+        raise ValueError(folder)
+
+    print(f'Creating Rekordbox playlist {rekordbox_full_name}: {len(track_ids_df)} tracks')
+    if print_tracks:
+        pretty_print_tracks(track_ids_df, indent=' ' * 4, enum=True)
+
+    rekordbox.create_playlist(rekordbox_full_name, track_ids_df, overwrite=overwrite)
+    rekordbox.write()
+
+    return
+
+
+def create_spotify_playlist(
+        name,
+        track_ids_df,
+        print_tracks=True,
+        spotify_prefix='DJ ',
+        spotify_overwrite=True
+):
+    rekordbox_to_spotify_mapping = docs['rekordbox_to_spotify'].read()
+
+    # remove empty mappings
+    rekordbox_to_spotify_mapping = rekordbox_to_spotify_mapping.loc[
+        ~pd.isna(rekordbox_to_spotify_mapping.spotify_id)
+    ]
+
+    spotify_ids_df = track_ids_df.merge(
+        rekordbox_to_spotify_mapping,
+        how='inner',
+        left_index=True,
+        right_index=True
+    )
+
+    if len(spotify_ids_df) < len(track_ids_df):
+        print('%d tracks are missing Spotify mappings; omitting.' % (
+                len(track_ids_df) - len(spotify_ids_df)
+        ))
+
+    spotify_playlist_name = spotify_prefix + name
+
+    print(f'Creating Spotify playlist {spotify_playlist_name}: {len(spotify_ids_df)} tracks')
+    if print_tracks:
+        pretty_print_tracks(track_ids_df, indent=' ' * 4, enum=True)
+
+    spotify_playlists = spotify.get_playlists()
+
+    if spotify_playlist_name in spotify_playlists.index:
+        spotify_playlist_id = spotify_playlists.at[spotify_playlist_name, 'id']
+
+        if not spotify_overwrite:
+            raise Exception("Spotify playlist '%s' already exists" % spotify_playlist_name)
+
+        spotify_playlist_tracks = spotify.get_playlist_tracks(spotify_playlist_id)
+
+        spotify_ids_to_remove = spotify_playlist_tracks.index.difference(
+            spotify_ids_df.spotify_id,
+            sort=False
+        )
+
+        spotify_ids_to_add = pd.Index(spotify_ids_df.spotify_id).difference(
+            spotify_playlist_tracks.index,
+            sort=False
+        )
+
+        print("Spotify playlist '%s' exists with %d tracks; removing %d tracks, adding %d tracks" % (
+            spotify_playlist_name,
+            len(spotify_playlist_tracks),
+            len(spotify_ids_to_remove),
+            len(spotify_ids_to_add)
+        ))
+
+        if len(spotify_ids_to_remove) > 0:
+            spotify.remove_tracks_from_playlist(spotify_playlist_id, spotify_ids_to_remove)
+        if len(spotify_ids_to_add) > 0:
+            spotify.add_tracks_to_playlist(spotify_playlist_id, spotify_ids_to_add)
+
+    else:
+        print("Creating Spotify playlist '%s' with %d tracks" % (
+            spotify_playlist_name,
+            len(spotify_ids_df)
+        ))
+
+        spotify.create_playlist(spotify_playlist_name)
+        spotify.add_tracks_to_playlist(spotify_playlist_name, spotify_ids_df.spotify_id)
+
+    return
+
+
 def playlist_maintenance(do_rekordbox=True, do_spotify=True):
     # build Main Library on Spotify; it already exists in Rekordbox
-    build_playlist(
+    create_playlist_from_condition(
         'Main Library',
         condition=lambda x: True,
         print_tracks=False,
@@ -178,8 +249,8 @@ def playlist_maintenance(do_rekordbox=True, do_spotify=True):
     )
 
     # build the class playlists
-    for playlist, condition in _playlists.items():
-        build_playlist(
+    for playlist, condition in _playlist_conditions.items():
+        create_playlist_from_condition(
             playlist,
             condition=condition,
             print_tracks=False,

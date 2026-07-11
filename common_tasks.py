1929f33925f7045cfcc19cf369ffb8e0e4c587e8
Spyros Triantafyllis <striantafyllis@gmail.com>
2026-07-10 18:53:01 -0700

    Utility method for adding Great American Songbook songs to songs L1

--- common_tasks.py
+++ common_tasks.py
@@ -70,3 +70,46 @@ def queue_maintenance_songs(last_track=None, **kwargs):
         **kwargs
     )
 
+great_american_songbook_csv = '/Users/spyros/Music/djlib/Great American Songbook with Spotify IDs.csv'
+
+def great_american_songbook_to_songs_queue(use_listening_history=True):
+    great_american_songbook = Doc(
+        name='Great American Songbook',
+        path=great_american_songbook_csv,
+        index_column='spotify_id'
+    )
+
+    great_american_songs = great_american_songbook.get_df()
+
+    # throw away the songs without a spotify ID; we can't handle these yet
+    great_american_songs = great_american_songs[great_american_songs.index.notna()]
+
+    # for each song, find the one with the max popularity score
+
+    groups = great_american_songs.groupby(['Year', 'Song title', 'Composer(s)'])
+
+    great_american_songs_max_pop = groups.apply(lambda group: group.loc[group['match_score'].idxmax()])
+
+    print(f'Found {len(great_american_songs_max_pop)} unique songs in Great American Songbook')
+
+    if use_listening_history:
+        listening_history = ListeningHistory()
+
+        # remove the songs in listening history
+        unlistened_great_american_songs_max_pop = great_american_songs_max_pop.loc[
+            ~great_american_songs_max_pop.index.isin(listening_history.get_df().index)
+        ]
+        print(f'{len(unlistened_great_american_songs_max_pop)} of these songs are unlistened')
+
+        great_american_songs_max_pop = unlistened_great_american_songs_max_pop
+
+    print(f'Adding {len(great_american_songs_max_pop)} songs to queue...')
+
+    queue = SpotifyPlaylist('songs L1')
+
+    queue.append(great_american_songs_max_pop)
+
+    print(f'songs L1 now has {len(queue)} songs')
+
+    queue.write()
+    return
